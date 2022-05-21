aee56e42e663fa2cf67e80deef13307ad0a451a5
Saurabh Sharma <saurabh.sharma4@bp.com>
2022-05-21 03:15:16 +0100

    Updated test class

---
+++ src/test/java/com/tsz/tdd/tdd/example/controller/LoginControllerTest.java
@@ -0,0 +1,43 @@
+package com.tsz.tdd.tdd.example.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebMvcTest(LoginController.class)
+@AutoConfigureMockMvc(addFilters = false)
+class LoginControllerTest
+{
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    private ObjectMapper objectMapper;
+
+    @BeforeEach
+    void setUp(){ objectMapper = new ObjectMapper();}
+
+    @Test
+    void shouldNotLoginWhenUserIdIsNull() throws Exception
+    {
+        final String userId = "saurabh.sharma4@bp.com";
+
+        mockMvc.perform(get("").accept(MediaTypes.HAL_FORMS_JSON_VALUE))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+}
