2d6d11dceea79b3e466754358da34a6d9e4fb433
Saurabh Sharma <saurabh.sharma4@bp.com>
2022-05-21 02:26:37 +0100

    Updated project to MicroserviceesTddApplication

---
+++ src/main/java/com/tsz/tdd/tdd/example/MicroserviceesTddApplication.java
@@ -0,0 +1,13 @@
+package com.tsz.tdd.tdd.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceesTddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceesTddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tsz/tdd/tdd/example/MicroserviceesTddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsz.tdd.tdd.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceesTddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
